ee0a824d8576a6671cb0a3d97072a550a880afd0
xiaokexiang <xxiaokexiang@gmail.com>
2025-03-05 16:33:51 +0800

    go-in-action-2025 map

--- hello.go
+++ hello.go
@@ -3,7 +3,7 @@ import (
 	"fmt"
 	pkg1 "go-in-action-2025/package1" // 给包设置别名
 	pkg2 "go-in-action-2025/package2"
-	_ "go-in-action-2025/package3" // 不使用包中的方法,只使用init\
+	_ "go-in-action-2025/package3" // 不使用包中的方法,只使用init
 	. "go-in-action-2025/package4" // 导入包下所有的公有方法,不推荐
 )
 
@@ -15,8 +15,77 @@ func main() {
 	//importInit()
 	//pointer()
 	//deferFunc()
-	arrOrSliceFunc()
+	//arrOrSliceFunc()
+	mapFunc()
 }
+
+/*
+map作为参数和slice一样是引用的副本，指向的数据是共享的，
+*/
+func mapFunc() {
+	myMap1 := map[string]string{
+		"one": "java",
+		"two": "go",
+	}
+	fmt.Printf("%p\n", myMap1)
+	myMap1["one"] = "java"
+	myMap1["two"] = "go"
+	fmt.Printf("myMap1: %v, addr: %p\n", myMap1, myMap1)
+	var myMap2 map[string]string
+	fmt.Printf("%p\n", myMap2)           // 0x0 即表示为nil，未指向任何有效的内存地址
+	myMap3 := make(map[string]string, 1) // make来开辟有效的内存空间
+	myMap3["one"] = "java"
+	fmt.Printf("size=%d, myMap3= %v, myMap3 addr=%p\n", len(myMap3), myMap3, myMap3)
+	myMap3["two"] = "go"
+	fmt.Printf("size=%d, myMap3= %v, myMap3 addr=%p\n", len(myMap3), myMap3, myMap3)
+
+	for key, value := range myMap3 {
+		fmt.Printf("key: %s, value: %s\n", key, value)
+	}
+	delete(myMap3, "one")
+	for key, value := range myMap3 {
+		fmt.Printf("key: %s, value: %s\n", key, value)
+	}
+}
+
+/*
+数组([N]int):
+
+arr := [3]int{1, 2, 3}
+内存布局：
++-------------------+
+| 1 | 2 | 3         |  <-- 数组内容
++-------------------+
+^
+|
+&arr 指向这里
+1. 值类型，创建是需要指定容量，作为参数传递的是值的拷贝，变量直接存储数组内容，需要&才能打印内存地址，否则是数组内容
++-------------------+
+
+slice([]int):
+
+	type slice struct {
+		array unsafe.Pointer // 存储了指向数组的指针
+		len   int
+		cap   int
+	}
+
+slice := []int{1, 2, 3}
+
+切片头（Slice Header）：
++-------------------+
+| 指针 | 长度 | 容量 |  <-- 切片头
++-------------------+
+
+	|
+	v
+
++-------------------+
+| 1 | 2 | 3         |  <-- 底层数组
++-------------------+
+1. 指针类型，作为参数传递的是指针，变量存储切片头（包含指针、长度、容量），不需要&，直接打印底层数组的地址
+2. make([]int, 3, 5) 开辟3个空间，slice的cap是5，size是3，当数量超过cap，会再次扩容一块区域，大小为5（开辟的地址不一定连续），如果不指定cap，默认等于size
+*/
 func arrOrSliceFunc() {
 	arrFunc()
 	mySlice := []int{1, 2, 3}
@@ -40,14 +109,9 @@ func arrFunc() {
 	for i, v := range arr2 {
 		fmt.Println("index: value: ", i, v)
 	}
+	fmt.Printf("arr2=%p\n", &arr2)
 }
 
-/*
-1. slice又称动态数组[]int,相比数组容量不固定,数组创建时需要指定容量.
-2. slice作为参数传递时, slice是引用传递而数组是值传递
-3. make([]int, 3, 5) 开辟3个空间，slice的cap是5，size是3，当数量超过cap，会再次扩容一块区域，大小为5（开辟的地址不一定连续）
-4. 如果不指定cap，默认等于size
-*/
 func sliceFunc(mySlice []int) {
 	for _, v := range mySlice {
 		fmt.Println("v: ", v)
@@ -89,12 +153,14 @@ func sliceFunc(mySlice []int) {
 	slice5 = slice4[:]    // 全部
 	slice4[0] = 100
 	fmt.Println(slice4)
+	fmt.Printf("slice4=%p\n", slice4)
 	fmt.Println(slice5) // s5和s4都会改变，他们指向的是同一数据
-
+	fmt.Printf("slice5=%p\n", slice5)
 	slice6 := make([]int, 3)
 	copy(slice6, slice5) // copy slice5指向的内存的数据，slice6指向新的地址空间
 	slice4[0] = 101
 	fmt.Println(slice6)
+	fmt.Printf("slice6=%p\n", slice6)
 }
 
 /*
